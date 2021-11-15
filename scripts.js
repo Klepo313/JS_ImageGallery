b73643d10c14e7a6b39e4283c0c09c03f16e0b14
TheKlepo75 <antonio.klepo1@gmail.com>
2021-11-15 12:27:47 +0100

    commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const slika1 = {
+    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
+    header: "Foggy forest",
+    location: "Switzerland"
+}
+const slika2 = {
+    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
+    header: "Lonely road",
+    location: "Švicarska"
+}
+const slika3 = {
+    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
+    header: "Maglovita šuma",
+    location: "Švicarska"
+}
+const slika4 = {
+    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
+    header: "Maglovita šuma",
+    location: "Švicarska"
+}
+const slika5 = {
+    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
+    header: "Maglovita šuma",
+    location: "Švicarska"
+}
+const slika6 = {
+    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
+    header: "Maglovita šuma",
+    location: "Švicarska"
+}
