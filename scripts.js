0d0f156a79ac9c48d587b9bcf76fb645098a7b6a
Klepo313 <antonio.klepo1@gmail.com>
2021-11-16 18:30:01 +0100

    commit

--- scripts.js
+++ scripts.js
@@ -4,14 +4,14 @@ const slika1 = {
     location: "Switzerland"
 }
 const slika2 = {
+    src: "images/john-towner-3Kv48NS4WUU-unsplash.jpg",
+    header: "Morning spring forest",
+    location: "Switzerland"
+}
+const slika3 = {
     src: "images/lukasz-szmigiel-jFCViYFYcus-unsplash.jpg",
     header: "Lonely road",
     location: "Denmark"
-}
-const slika3 = {
-    src: "images/jay-mantri-TFyi0QOx08c-unsplash.jpg",
-    header: "Morning spring forest",
-    location: "Switzerland"
 }
 const slika4 = {
     src: "images/sebastian-unrau-sp-p7uuT0tw-unsplash.jpg",
@@ -28,3 +28,12 @@ const slika6 = {
     header: "The tree of life",
     location: "Ganna"
 }
+
+var mainSlika = document.getElementById("mainSlika")
+var h2 = document.getElementsByTagName("h2")[0]
+var location = document.getElementsByClassName("location")[0]
+
+mainSlika.src = slika1.src;
+h2.innerHTML = slika1.header;
+location.innerHTML = slika1.location;
+
